e690673bb14cf8bd381441d872251168949f1c88
Takumi130902 <b1020239@fun.jp>
2022-10-10 15:26:29 +0900

    カウンターをタップで数字が増加しFizzBuzzを正確に表示

--- FizzBuzz/FizzBuzz/ViewController.swift
+++ FizzBuzz/FizzBuzz/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     //ラベルのインスタンス
     let label = UILabel()
+    let labelCount = UILabel()
     //カウンター
     var count = 0
     
@@ -49,20 +50,26 @@ class ViewController: UIViewController {
                          for: .touchUpInside)
         
         // ラベルのサイズを設定
-        label.frame = CGRect(x:screenWidth/4, y:200, width:screenWidth/2, height:50);
+        label.frame = CGRect(x:screenWidth/4, y:600, width:screenWidth/2, height:50);
+        labelCount.frame = CGRect(x:screenWidth/4, y:200, width:screenWidth/2, height:50);
         // ラベルの文字を設定
         label.text = "0"
+        labelCount.text = "0"
         // 文字を中央にalignする
         label.textAlignment = NSTextAlignment.center
+        labelCount.textAlignment = NSTextAlignment.center
         // ラベルのフォントサイズ
         label.font = UIFont.systemFont(ofSize: 36)
+        labelCount.font = UIFont.systemFont(ofSize: 36)
         // Viewにラベルを追加
         self.view.addSubview(label)
+        self.view.addSubview(labelCount)
     }
     
     @objc func buttonTapped(sender: Any) {
         count += 1
         label.text = fizzBuzzCheck(i: count)
+        labelCount.text = String(count)
     }
     
     /*
