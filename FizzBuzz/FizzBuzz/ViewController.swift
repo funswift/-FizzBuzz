7abf22776df712379f2c8a41648e57fc22dae988
ami Nitadori <nmeowkso0403@icloud.com>
2022-10-11 18:23:04 +0900

    2つ目のタスク

--- FizzBuzz/FizzBuzz/ViewController.swift
+++ FizzBuzz/FizzBuzz/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var input: UILabel!
+    var i:Int=0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        input.text=String(i)
         // Do any additional setup after loading the view.
+        /*
       for i in 1...100{
           if(i%3==0 && i%5==0){
               print("FizzBuzz")
@@ -22,9 +27,13 @@ class ViewController: UIViewController {
           }else{
               print (i)
           }
-      }
-      
+      }*/
     }
 
+    @IBAction func countup(_ sender: UIButton) {
+        i=i+1
+        input.text=String(i)
+        
+    }
 }
 
