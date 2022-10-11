278fbf7cc6ba87dbc6732141713339dca3a5ccd1
ami Nitadori <nmeowkso0403@icloud.com>
2022-10-11 17:41:50 +0900

    1つ目のタスク改訂

--- FizzBuzz/FizzBuzz/ViewController.swift
+++ FizzBuzz/FizzBuzz/ViewController.swift
@@ -13,11 +13,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
       for i in 1...100{
-        print (i)
+          if(i%3==0 && i%5==0){
+              print("FizzBuzz")
+          }else if(i%3==0){
+              print("Fizz")
+          }else if(i%5==0){
+              print("Buzz")
+          }else{
+              print (i)
+          }
       }
       
     }
-
 
 }
 
