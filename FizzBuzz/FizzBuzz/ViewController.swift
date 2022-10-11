c1451bf6e0e0f732b865c735860c31dd0aeea319
Mugi <mugi@MuginoMacBook-Pro.local>
2022-10-11 15:14:28 +0900

    画面上でボタンを押すと、数字が増える

--- FizzBuzz/FizzBuzz/ViewController.swift
+++ FizzBuzz/FizzBuzz/ViewController.swift
@@ -9,22 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    
+    @IBOutlet weak var countLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        for i in 1...100{
-              switch (i % 3, i % 5){
-                  case (0, 0) :
-                    print("FizzBuzz")
-                  case (0, _) :
-                    print("Fizz")
-                  case (_, 0) :
-                    print("Buzz")
-                default:
-                  print(i)
-              }
-
-        }
+        
+    }
+    @IBAction func countButton(_ sender: Any) {
+        count = count + 1
+        countLabel.text = String(count)
     }
 }
 
+
+//for i in 1...100{
+//      switch (i % 3, i % 5){
+//          case (0, 0) :
+//            print("FizzBuzz")
+//          case (0, _) :
+//            print("Fizz")
+//          case (_, 0) :
+//            print("Buzz")
+//        default:
+//          print(i)
+//      }
+//
+//}
